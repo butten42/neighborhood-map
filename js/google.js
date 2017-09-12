dad9affaff3591128906837f38ef57cceee588ee
butten42 <butten42@hotmail.com>
2017-09-12 20:41:54 +0800

    a

--- js/google.js
+++ js/google.js
@@ -1,66 +1,3 @@
-var map;
-var markers = ko.observableArray([]);
-var locations = [{
-      title: 'Shenzhen Library of Science and Technology',
-      address: "中国广东省深圳市南山区丽水路",
-      tel: '0755 2603 2921',
-      location: {
-        lat: 22.5922326,
-        lng: 113.9706436
-      },
-      placeId: 'ChIJlck5HozyAzQRlmYOSLbOQE4'
-    },
-    {
-      title: 'Zoo',
-      address: '中国广东省深圳市南山区西丽路',
-      tel: '0755 2662 2888',
-      location: {
-        lat: 22.5923205,
-        lng: 113.9646499
-      },
-      placeId: 'ChIJw33RSpLyAzQRonD4xuEQczQ'
-    },
-    {
-      title: 'The metro',
-      address: "中国广东省深圳市南山区雨水路",
-      tel: '0755 2603 2955',
-      location: {
-        lat: 22.581893,
-        lng: 113.9629383
-      },
-      placeId: 'ChIJlck5HozyAzQRlmYOSLbOQE4'
-    },
-    {
-      title: 'Liao',
-      address: "中国广东省深圳市南山区贵喵路",
-      tel: '0755 2603 2921',
-      location: {
-        lat: 22.5831015,
-        lng: 113.9642472
-      },
-      placeId: 'ChIJtegeoRXuAzQRQi9n4dn9w-0'
-    },
-    {
-      title: 'The house of noodles',
-      address: "中国广东省深圳市南山区格于路",
-      tel: '0755 8240 1720',
-      location: {
-        lat: 22.5859439,
-        lng: 113.967487
-      },
-      placeId: 'ChIJ4X1Eeqn1AzQRju9J1yD0-Zc'
-    },
-    {
-      title: 'A hotel I never been',
-      address: "中国广东省深圳市南山区打的路",
-      tel: '0755 2866 5555',
-      location: {
-        lat: 22.585762,
-        lng: 113.9639131
-      },
-      placeId: 'ChIJr5fS66t1BDQR8dpM0UXx7r8'
-    }
-  ];
 
 function initMap() {
   var SZ = {
@@ -79,22 +16,21 @@ function initMap() {
     var position = locations[i].location;
     var title = locations[i].title;
     var placeId = locations[i].placeId;
-    var marker = new google.maps.Marker({
+     marker = new google.maps.Marker({
       position: position,
       title: title,
       animation: google.maps.Animation.DROP,
       id: placeId
     });
-
-    markers().push(marker);
-
+     locations[i].marker=marker;
+    markers.push(marker);
     marker.addListener('click', function () {
       populateInfoWindow(this, largeInfowindow);
     });
   }
   showListings();
+ko.applyBindings(new ViewModel());
 }
-
 function populateInfoWindow(marker, infowindow) {
   var service = new google.maps.places.PlacesService(map);
   service.getDetails({
@@ -130,7 +66,6 @@ function populateInfoWindow(marker, infowindow) {
       innerHTML += '</div>';
       infowindow.setContent(innerHTML);
       infowindow.open(map, marker);
-      // Make sure the marker property is cleared if the infowindow is closed.
       infowindow.addListener('closeclick', function () {
         infowindow.marker = null;
       });
@@ -139,16 +74,9 @@ function populateInfoWindow(marker, infowindow) {
 }
 function showListings() {
         var bounds = new google.maps.LatLngBounds();
-        // Extend the boundaries of the map for each marker and display the marker
-        for (var i = 0; i < markers().length; i++) {
-          markers()[i].setMap(map);
-          bounds.extend(markers()[i].position);
+        for (var i = 0; i < markers.length; i++) {
+          markers[i].setMap(map);
+          bounds.extend(markers[i].position);
         }
         map.fitBounds(bounds);
       }
-function hideListings() {
-  for (var i = 0; i < markers().length; i++) {
-    markers()[i].setMap(null);
-  }
-}
-
--- js/main.js
+++ js/main.js
@@ -9,10 +9,84 @@ $('#iconModal').modal('hide');
 $('#rightButton').click(function(){
 $('#iconModal').modal('hide');
 });
+var map;
+var marker;
+var markers = [];
+var locations = [{
+      title: 'Shenzhen Library of Science and Technology',
+      address: "中国广东省深圳市南山区丽水路",
+      tel: '0755 2603 2921',
+      location: {
+        lat: 22.5922326,
+        lng: 113.9706436
+      },
+      placeId: 'ChIJlck5HozyAzQRlmYOSLbOQE4'
+    },
+    {
+      title: 'Zoo',
+      address: '中国广东省深圳市南山区西丽路',
+      tel: '0755 2662 2888',
+      location: {
+        lat: 22.5923205,
+        lng: 113.9646499
+      },
+      placeId: 'ChIJw33RSpLyAzQRonD4xuEQczQ'
+    },
+    {
+      title: 'The metro',
+      address: "中国广东省深圳市南山区雨水路",
+      tel: '0755 2603 2955',
+      location: {
+        lat: 22.581893,
+        lng: 113.9629383
+      },
+      placeId: 'ChIJlck5HozyAzQRlmYOSLbOQE4'
+    },
+    {
+      title: 'Liao',
+      address: "中国广东省深圳市南山区贵喵路",
+      tel: '0755 2603 2921',
+      location: {
+        lat: 22.5831015,
+        lng: 113.9642472
+      },
+      placeId: 'ChIJtegeoRXuAzQRQi9n4dn9w-0'
+    },
+    {
+      title: 'The house of noodles',
+      address: "中国广东省深圳市南山区格于路",
+      tel: '0755 8240 1720',
+      location: {
+        lat: 22.5859439,
+        lng: 113.967487
+      },
+      placeId: 'ChIJ4X1Eeqn1AzQRju9J1yD0-Zc'
+    },
+    {
+      title: 'A hotel I never been',
+      address: "中国广东省深圳市南山区打的路",
+      tel: '0755 2866 5555',
+      location: {
+        lat: 22.585762,
+        lng: 113.9639131
+      },
+      placeId: 'ChIJr5fS66t1BDQR8dpM0UXx7r8'
+    }
+  ];
+function loadScript(url){
+    var Script = document.createElement('script');
+    Script.setAttribute('src', url);
+    document.body.appendChild(Script);
+}
+var googleScript='js/google.js';
+var googleUrl="https://maps.googleapis.com/maps/api/js?libraries=places&key=AIzaSyAREfcIaFiSSJ81sB884G9GvdNqp6Y_FjE&v=3&callback=initMap";
+loadScript(googleScript);
+loadScript(googleUrl);
 var Location=function(data){
-    this.title = ko.observable(data.title);
-    this.tel= ko.observable(data.tel);
-    this.address=ko.observable(data.address);
+    this.title = data.title;
+    this.tel= data.tel;
+    this.address=data.address;
+    this.marker=data.marker;
 }
 
 var ViewModel = function(){
@@ -21,46 +95,19 @@ var ViewModel = function(){
     locations.forEach(function(item){
         self.locationList.push(new Location(item));
     });
-    this.setLocation=function(clicked){
-        console.log(clicked);
+    this.setLocation=function(elem){
+      google.maps.event.trigger(elem.marker, "click");
     }
-    /*this.query=ko.observable('');
-    self.markers = ko.dependentObservable(function() {
-    var search = this.query().toLowerCase();
-    ko.utils.arrayFilter(markers, function(marker) {
-    if (marker.title.toLowerCase().indexOf(search) >= 0) {
-            return marker.visible(true);
+    this.query=ko.observable('');
+    this.searchResults = ko.computed(function(){
+    var q = self.query().toLowerCase();
+    return ko.utils.arrayFilter(locations, function(item) {
+      if (item.title.toLowerCase().indexOf(q) >= 0) {
+            return item.setVisible(true);
         } else {
-            return marker.visible(false);
+            return item.setVisible(false);
         }
     });
-}, viewModel);*/
+  });
 }
-ko.applyBindings(new ViewModel());
-/* abandened
-var initialLocations= [];
-var markerid=['ChIJlck5HozyAzQRlmYOSLbOQE4','ChIJw33RSpLyAzQRonD4xuEQczQ','ChIJlck5HozyAzQRlmYOSLbOQE4','ChIJtegeoRXuAzQRQi9n4dn9w-0','ChIJ4X1Eeqn1AzQRju9J1yD0-Zc','ChIJr5fS66t1BDQR8dpM0UXx7r8']
-for (var i=0;i<markerid.length;i++){
-    initialLocations.push(getGoogles(markerid[i]));
-}
-function getGoogles(marker) {
-      var service = new google.maps.places.PlacesService(map);
-      service.getDetails({
-        placeId: marker
-      }, function(place, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-          var inner= {};
-          if (place.name) {
-            inner.name= place.name;
-          }
-          if (place.formatted_address) {
-            inner.address=place.formatted_address;
-          }
-          if (place.formatted_phone_number) {
-            inner.tel=place.formatted_phone_number;
-          }
-          return inner;
-        }
-      });
-    }
-*/
+
